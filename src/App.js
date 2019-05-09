5cc2e8412be269ca5d44aa930de628e95f25a039
Brian G <brian.gregorius@gmail.com>
2019-05-09 16:52:47 -0400

    added styles to app.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import React from "react";
-import "./App.css";
+
 import CounterContainer from "./components/counterContainer";
+import {
+  createMuiTheme,
+  MuiThemeProvider,
+  withStyles
+} from "@material-ui/core";
+import { styles } from "./components/styles/containerStyles";
 
-function App() {
+const theme = createMuiTheme({
+  typography: {
+    useNextVariants: true
+  },
+  palette: {
+    secondary: {
+      main: "#E0F4F4"
+    },
+    primary: {
+      main: "#2B3636"
+    }
+  }
+});
+
+const App = props => {
+  const { classes } = props;
   return (
-    <div className="App">
-      <CounterContainer />
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className={classes.root}>
+        <CounterContainer />
+      </div>
+    </MuiThemeProvider>
   );
-}
+};
 
-export default App;
+export default withStyles(styles)(App);
